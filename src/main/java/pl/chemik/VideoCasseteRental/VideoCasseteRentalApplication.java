5733e0e5ee90958f660321cf8164bc133d50ea5c
DJ Chemik <dj-chemik@wp.pl>
2020-03-04 21:16:56 +0100

    Add gitignore file

---
+++ src/main/java/pl/chemik/VideoCasseteRental/VideoCasseteRentalApplication.java
@@ -0,0 +1,13 @@
+package pl.chemik.VideoCasseteRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCasseteRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCasseteRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/chemik/VideoCasseteRental/VideoCassetteRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.chemik.VideoCasseteRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCassetteRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
